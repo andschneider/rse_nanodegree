c91ed18d491e5f0ddc03ae26b15218c55a4b84e4
Andrew Schneider <aschne93@gmail.com>
2019-03-03 14:19:59 -0800

    threshold based on slider values

--- ball_chaser/src/image_converter.cpp
+++ ball_chaser/src/image_converter.cpp
@@ -10,10 +10,29 @@ static const std::string CONVERTED_WINDOW = "converted window";
 static const std::string THRESHOLD_WINDOW = "threshold window";
 static const std::string CONTOUR_WINDOW = "contour window";
 
+const int slider_max = 255;
+const int hue_max = 180;
+int slider_lower_hue = 150;
+int slider_upper_hue = 180;
+int slider_lower_saturation = 250;
+int slider_lower_value = 250;
+int slider_upper_saturation = 255;
+int slider_upper_value = 255;
+
 cv::Mat cv_hsv;
 cv::Mat cv_grey;
 cv::Mat thresh_img;
 cv::Mat cv_bgr;
+
+void threshold_img(int, void*) {
+  // threshold based on a given range
+  cv::Scalar thresh_min =
+      cv::Scalar(slider_lower_hue, slider_lower_saturation, slider_lower_value);
+  cv::Scalar thresh_max =
+      cv::Scalar(slider_upper_hue, slider_upper_saturation, slider_upper_value);
+  /* std::cout << slider_lower_value << slider_upper_value << std::endl; */
+  inRange(cv_hsv, thresh_min, thresh_max, thresh_img);
+}
 
 class ImageConverter {
   ros::NodeHandle nh_;
@@ -46,12 +65,26 @@ class ImageConverter {
     cv_bgr = cv_ptr->image;
     // convert to hsv
     cv::cvtColor(cv_bgr, cv_hsv, cv::COLOR_BGR2HSV);
-    // threshold based on a given range
-    cv::Scalar thresh_min = cv::Scalar(0, 0, 250);
-    cv::Scalar thresh_max = cv::Scalar(6, 6, 255);
-    inRange(cv_hsv, thresh_min, thresh_max, thresh_img);
 
     cv::imshow(RAW_WINDOW, cv_ptr->image);
+    
+    // create sliders for selecting HSV threshold values
+    cv::createTrackbar("lower hue", RAW_WINDOW, &slider_lower_hue, hue_max,
+                       threshold_img);
+    cv::createTrackbar("upper hue", RAW_WINDOW, &slider_upper_hue, hue_max,
+                       threshold_img);
+
+    cv::createTrackbar("lower saturation", RAW_WINDOW, &slider_lower_saturation,
+                       slider_max, threshold_img);
+    cv::createTrackbar("upper saturation", RAW_WINDOW, &slider_upper_saturation,
+                       slider_max, threshold_img);
+
+    cv::createTrackbar("lower value", RAW_WINDOW, &slider_lower_value,
+                       slider_max, threshold_img);
+    cv::createTrackbar("upper value", RAW_WINDOW, &slider_upper_value,
+                       slider_max, threshold_img);
+    threshold_img(0, 0);
+
     cv::imshow(CONVERTED_WINDOW, cv_hsv);
     cv::imshow(THRESHOLD_WINDOW, thresh_img);
 
@@ -94,7 +127,7 @@ class ImageConverter {
     else if (center_x <= img_width)
       driving = RIGHT;
 
-    std::cout << driving << std::endl;
+    /* std::cout << driving << std::endl; */
 
     cv::waitKey(3);
 
