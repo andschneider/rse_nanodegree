c2982a6d3b0424dc800e719ee01bda5a0dd378e3
Andrew Schneider <aschne93@gmail.com>
2019-05-18 15:21:44 -0700

    Update launch script
    Using the default .launch files from turtlebot_gazebo and passing in parameters on call

--- home_service_robot/scripts/home_service.sh
+++ home_service_robot/scripts/home_service.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-xterm -e "source devel/setup.bash; roslaunch home_service_robot turtlebot.launch" &
+xterm -e "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find home_service_robot)/worlds/myworld.world" &
 sleep 5
 
-xterm -e "roslaunch home_service_robot amcl.launch" &
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find home_service_robot)/maps/map.yaml initial_pose_a:=4.71" &
 sleep 5
 
 xterm -e "roslaunch home_service_robot view_navigation.launch" &
